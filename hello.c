a8fe14bd3249ef8b6e3888401f9ab85463a6b4f3
judgment12 <38927727+judgment12@users.noreply.github.com>
2020-08-24 17:31:02 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*
+Author:
+Matthew Toland
+dtoland2@huskers.unl.edu
+
+Date:
+2020/08/24
+
+Program stating name
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+	
+	printf("Matthew Toland");
+	
+	return(0);
+}
